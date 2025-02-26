6b9e88332145b65703856e7267548244a93a9c4a
Duncan Jones <dj223@outlook.com>
2025-02-26 21:20:32 +0000

    Successfully connects to server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
-use std::net::ToSocketAddrs;
+use std::io::{Error, Write};
+use std::net::{TcpStream, ToSocketAddrs};
 use std::path::Path;
 
 const PORT: u32 = 80;
@@ -9,7 +10,21 @@ fn program_name(path : &String) -> String {
     Path::new(path.as_str()).file_name().unwrap().to_str().unwrap().to_string()
 }
 
+fn get_ip_addresses(dns: &str) -> Result<Vec<String>, Error> {
+    let ip_addresses = format!("{}:{}", dns, PORT).to_socket_addrs()?;
 
+    Ok(ip_addresses.map(|address| address.ip().to_string())
+        .collect()
+    )
+}
+
+fn initialise_connection(ip: &str) -> Result<TcpStream, Error> {
+    TcpStream::connect(format!("{}:{}", ip, PORT))
+}
+
+fn send_data(mut tcp_stream: TcpStream, dns: &str) -> Result<(), Error> {
+    tcp_stream.write_all(format!("{} {}", b"GET / HTTP/1.1\r\nHost: {}\r\n\r\n", dns).as_bytes())
+}
 
 
 fn main() {
@@ -20,15 +35,21 @@ fn main() {
     }
     let dns = &args[1];
     println!("Searching ips for: {}...", dns);
-    let ip_addresses = format!("{}:{}", dns, PORT).to_socket_addrs();
+    let ip_addresses = get_ip_addresses(&dns);
     match ip_addresses {
         Ok(addresses) => {
-            for addr in addresses {
-                println!("{}", addr.to_string());
+            for address in addresses {
+                let connection = initialise_connection(&address);
+                if let Ok(_) = connection {
+                    println!("Connection successful on IP: {}", address);
+                    break;
+                }else{
+                    println!("Failed to connect to IP: {}", address);
+                }
             }
         }
         Err(e) => {
-            eprintln!("Failed to find ip addresses! {}", e);
+            eprintln!("Error: {}", e);
         }
     }
 }
