1c6cef8278ced4400755c83cce5dd315a30d4369
Duncan Jones <dj223@outlook.com>
2025-03-09 19:02:40 +0000

    Normal is up to spec

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-use std::{env, thread};
-use std::io::{Error, Write};
+use std::{env, io, thread};
+use std::io::{Error, Read, Write};
 use std::net::{TcpStream, ToSocketAddrs};
 use std::path::Path;
 use std::sync::mpsc;
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{Receiver, Sender};
 
 const PORT: u32 = 80;
 
-
-struct DnsTcpStream {
-    stream: TcpStream,
-    ip: String,
-    dns: String
-}
 
 fn program_name(path : &String) -> String {
     Path::new(path.as_str()).file_name().unwrap().to_str().unwrap().to_string()
@@ -26,14 +20,53 @@ fn get_ip_addresses(dns: &str) -> Result<Vec<String>, Error> {
     )
 }
 
-fn initialise_connection(ip: &str) -> Result<DnsTcpStream, Error> {
+fn initialise_connection(ip: &str) -> Result<TcpStream, Error> {
     let stream = TcpStream::connect(format!("{}:{}", ip, PORT))?;
-    Ok(DnsTcpStream{stream, ip: ip.to_string(), dns: String::new()})
+    Ok(stream)
 }
 
-fn send_data(mut tcp_stream: TcpStream, dns: &str) -> Result<(), Error> {
-    let message = format!("{} {}", "GET / HTTP/1.1\r\nHost: {}\r\n\r\n", dns);
+fn send_data(mut tcp_stream: &TcpStream, dns: &str) -> Result<(), Error> {
+    let message = format!("GET / HTTP/1.1\r\nHost: {}\r\n\r\n", dns);
     tcp_stream.write_all(message.as_bytes())
+}
+
+fn read_data(mut tcp_stream: &TcpStream) -> Result<String, Error> {
+    let mut buffer = Vec::new();
+    tcp_stream.read_to_end(&mut buffer)?;
+
+    let response = String::from_utf8(buffer)
+        .map_err(|e| Error::new(io::ErrorKind::InvalidData, e))?;
+
+    Ok(response)
+}
+
+fn receiver_executor_wrapper(receiver_channel: Receiver<TcpStream>, transmitter_channel: Sender<TcpStream>, dns: &str) -> Result<(), Error> {
+    while let Ok(dns_tcp_stream) = receiver_channel.recv() {
+        if send_data(&dns_tcp_stream, &dns).is_ok(){
+            drop(transmitter_channel);
+            let data = read_data(&dns_tcp_stream)?;
+            println!("{}", data);
+            break;
+        } else{
+            eprintln!("Tcp stream failed to send data!");
+        }
+    }
+    Ok(())
+}
+
+fn worker_executor_wrapper(ip_address: &str, transmitter_channel: Sender<TcpStream>, start_channel: Receiver<()>) -> Result<(), Error> {
+    if start_channel.recv().is_err() {
+        return Err(Error::new(io::ErrorKind::Other, "Failed to receive start!"));
+    }
+
+    let tcp_stream = initialise_connection(&ip_address)?;
+
+    if transmitter_channel.send(tcp_stream).is_err() {
+        return Err(Error::new(io::ErrorKind::Other, "Failed to send tcp_stream!"));
+    }
+
+
+    Ok(())
 }
 
 
@@ -53,36 +86,32 @@ fn main() {
     let ip_addresses = ip_addresses_res.unwrap();
 
     let mut thread_handles = Vec::with_capacity(ip_addresses.len());
+    let mut start_channels = Vec::with_capacity(ip_addresses.len());
+
     let (tx, rx) = mpsc::channel();
-    let tx_clone: Sender<DnsTcpStream> = tx.clone();
-    let reciever = thread::spawn(move || {
-        while let Ok(dns_tcp_stream) = rx.recv() {
-            println!("Sent data!");
-            if let Err(error) = send_data(dns_tcp_stream.stream, &dns){
-                eprintln!("Error ({}): {}", dns_tcp_stream.ip, error);
-            } else{
-                println!("Data sent successfully to {}!", dns_tcp_stream.ip);
-                drop(tx);
-                break;
-            }
+    let tx_clone = tx.clone();
+
+    let receiver = thread::spawn(move || {
+        match receiver_executor_wrapper(rx, tx_clone, &dns) {
+            Ok(_) => {},
+            Err(e) => {eprintln!("Error: {}", e); return;}
         }
     });
-    println!("Ip addresses: {:?}", ip_addresses);
+
+
     for ip_address in ip_addresses {
-        let tx_cpy = tx_clone.clone();
+        let (stx, srx) = mpsc::channel();
+        start_channels.push(stx);
+        let tx_cpy = tx.clone();
         thread_handles.push(thread::spawn(move || {
-            if let Ok(dns_tcp_stream) = initialise_connection(&ip_address){
-                // dns_tcp_stream.dns = dns;
-                if let Err(e) = tx_cpy.send(dns_tcp_stream){
-                    eprintln!("Error: {}", e);
-                }
-            }
+            worker_executor_wrapper(&*ip_address, tx_cpy, srx)
         }));
     }
 
-
-    for handle in thread_handles {
-        handle.join().unwrap();
+    for start_channel in start_channels {
+        start_channel.send(()).unwrap();
     }
-    reciever.join().unwrap();
+
+    receiver.join().unwrap();
+
 }
